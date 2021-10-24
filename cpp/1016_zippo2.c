c4ebf78d74429757a24ff38e76e83abbdb632072
caddy <caddyRen@qq.com>
2021-10-24 23:52:14 +0800

    feat: 指针之间赋值要注意类型和const修饰符

--- cpp/1016_zippo2.c
+++ cpp/1016_zippo2.c
@@ -14,5 +14,16 @@ int main(void)
 	printf(" **pz=%d\n",**pz);
 	printf("    pz[2][1]=%d\n",pz[2][1]);
 	printf("*(*(pz+2)+1)=%d\n",*(*(pz+2)+1));
+	
+	printf("指针兼容性\n");
+	const int **pp2;//指向int指针的指针
+	int *p1;//指向int的指针
+	const int n=13;
+	pp2=&p1;//导致const int **pp2 的const失效
+	*pp2=&n;//导致p1指向n
+	printf("pp2=%p,*pp2=%p,p1=%p\n",pp2,*pp2,p1);
+	printf("p1=%p,&n=%p\n",p1,&n);
+	*p1=10;//此时n值无法确定结果,不同编译器n结果不一样 gcc n=13 和clang n=10
+	printf("n=%d\n",n);
 	return 0;
 }
