357588d9807da8f474b2c937f7e53e3654c54bec
caddy <caddyRen@qq.com>
2021-11-10 00:27:12 +0800

    feat: 安全的字符串转数值

--- cpp/1132_hello.c
+++ cpp/1132_hello.c
@@ -1,4 +1,15 @@
-/* hello.c -- 命令行参数转数字 atoi() */
+/* hello.c -- 命令行参数转数字 
+ *
+ * stdlib.h
+ * atoi()
+ * atoi("42regular") 返回42
+ * atoi("what") 返回0 但是C标准规定这种情况下的行为是未定义的，不安全
+ *
+ * 类似的函数有 都不安全
+ * atoi() 把字符串转换成int
+ * atof() 把字符串转换成double类型
+ * atol() 把字符串转换成long类型
+ * */
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc,char *argv[])
